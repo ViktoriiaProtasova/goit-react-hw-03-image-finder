5f128a08846078434851ab26b384765b1ee2267f
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-07-11 19:43:52 +0300

    Updated files according to the task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+
+class App extends Component {
+  state = {
+    searchQuery: '',
+  };
+
+  handleFormSubmit = searchQuery => {
+    this.setState({ searchQuery });
+  };
+
+  render() {
+    const { searchQuery } = this.state;
+    return (
+      <div>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ImageGallery searchQuery={searchQuery} />
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
